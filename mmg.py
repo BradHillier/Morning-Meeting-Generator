8485c3d405fcdb64c3ee930af508dc667f3d707a
Brad Hillier <bradhillier@Brads-MacBook-Air.local>
2021-05-06 16:58:11 -0700

    moved fetch tides to it's own function

--- mmg.py
+++ mmg.py
@@ -1,14 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
-TIDES_URL = 'https://tides.gc.ca/eng/station?sid=7460'
+
+def fetch_tides() -> list:
+    """
+    return list containing tuples of the format (time: str, height: str);
+    tides will be in order of earliest to latest
+    """
+    res = requests.get('https://tides.gc.ca/eng/station?sid=7460')
+    html = res.content
+    soup = BeautifulSoup(html, 'html.parser')
+
+    time = soup.table.tbody.findAll('td', class_='time')
+    height = soup.table.tbody.findAll('td', class_='heightMeters')
+
+    return [(time[i].text, height[i].text) for i in range(len(time))]
+
+tides = fetch_tides()
+for tide in tides:
+    t, h = tide
+    print(f'Time: {t}, Height: {h}')
 
 
-req = requests.get(TIDES_URL)
-html = req.content
-soup = BeautifulSoup(html, 'html.parser')
 
-tide_time = soup.table.tbody.findAll('td', class_='time')
-tide_height = soup.table.tbody.findAll('td', class_='heightMeters')
-tides = [(tide_time[i].text, tide_height[i].text) for i in range(len(tide_time))]
-print(tides)
