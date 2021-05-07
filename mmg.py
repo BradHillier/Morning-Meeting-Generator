52b73b510f0476f97dddd00d7d4da9a31a8e0108
Brad Hillier <bradhillier@Brads-MacBook-Air.local>
2021-05-06 23:48:59 -0700

    added ability to generate document containing weather and tides from template

--- mmg.py
+++ mmg.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 from docx import Document
 from docx.text.paragraph import Paragraph
+from docxtpl import DocxTemplate
 import requests
+
+
+def main():
+    create_document()
 
 
 def get_tides() -> list:
@@ -44,7 +49,34 @@ def get_weather() -> list:
     return hourly
 
 
+def create_document():
+    template = DocxTemplate('morning_meeting_template.docx')
+
+    tides = get_tides()
+    table_contents = []
+    for time, height in tides:
+        table_contents.append({
+            'Time': time, 
+            'Height': height
+        })
+
+    weather = get_weather()
+    weather_table_contents = []
+    for time, temp, cond, percip, wind in weather:
+        weather_table_contents.append({
+            'Time': time,
+            'Temp': temp,
+            'Conditions': cond,
+            'Wind': wind
+        })
+
+    context = {
+        'table_contents': table_contents,
+        'weather_table_contents': weather_table_contents
+    }
+    template.render(context)
+    template.save('generated_meeting.docx')
 
 
-
-
+if __name__ == '__main__':
+    main()
