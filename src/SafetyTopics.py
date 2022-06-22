2902fb2f3c08ac3ab262ae55af5b70565c518324
BradHillier <bradhillier@stumail.viu.ca>
2022-06-22 14:44:30 -0700

    added high uv warning to safety topics; added launch.json for debugging in vs code

--- src/SafetyTopics.py
+++ src/SafetyTopics.py
@@ -2,6 +2,8 @@
 # and should moved into the config file
 # such as:
 #  temp for heat exhaustion warning
+from time import strftime
+from Weather import Weather
 
 class SafetyTopics:
 
@@ -23,6 +25,7 @@ class SafetyTopics:
       '''
       self.heat_exhaustion()
       self.tide_too_low_for_woods()
+      self.uv_index()
 
    def heat_exhaustion(self):
 
@@ -42,3 +45,33 @@ class SafetyTopics:
          pt1 = 'Tides too low to access channel behind Woods Island'
          pt2 = f' from {start} to {end}'
          self.topics.append(pt1 + pt2)
+
+   def uv_index(self):
+      danger = 3;
+      dangerous_hours = list(filter(lambda hr : hr.uv >= danger, self.doc.weather))
+      if (len(dangerous_hours) > 0):
+         for split in split_hours(dangerous_hours):
+            warning = f'Wear sunscreen. The UV-index is high today from {split[0].date.strftime("%-I %p")} until {split[-1].date.strftime("%-I %p")}'
+            self.topics.append(warning)
+            
+
+# needs to be tested still
+def split_hours(hrs):
+   """
+   takes a list of Weather objects and splits them into groups of consecutive hours
+   i.e. Weather objects whose hours are as follows: 
+      [1, 2, 5, 6, 9] would be split into [[1, 2], [5, 6], [9]]
+   """
+   splits = list()
+   curr_split = list()
+   for hr in hrs:
+      if (len(curr_split) == 0) or (int(curr_split[-1].date.hour) + 1 == int(hr.date.hour)):
+         curr_split.append(hr)
+      else:
+         splits.append(curr_split)
+         curr_split.clear()
+   splits.append(curr_split)
+   return splits
+
+
+      
